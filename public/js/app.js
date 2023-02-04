74e02cdeb4b8fa5505570c0d2132f06d03439e96
Lukas Roth <37404152+lukasroth@users.noreply.github.com>
2023-02-05 00:50:36 +0100

    remove unnecessary console log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,7 @@
-console.log('Client side javascript fille is loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
